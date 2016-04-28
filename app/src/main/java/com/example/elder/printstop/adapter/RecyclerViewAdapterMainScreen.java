81de81235c7358775109ae089d90b2e42222e1e8
eldercampanha <eldercampabaltazar@gmail.com>
2016-04-27 23:56:19 -0300

    merging

--- app/src/main/java/com/example/elder/printstop/adapter/RecyclerViewAdapterMainScreen.java
+++ app/src/main/java/com/example/elder/printstop/adapter/RecyclerViewAdapterMainScreen.java
@@ -2,10 +2,13 @@ package com.example.elder.printstop.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.graphics.pdf.PdfRenderer;
+import android.net.Uri;
 import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.support.v7.widget.RecyclerView;
@@ -24,6 +27,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Elder on 3/5/2016.
@@ -63,41 +67,61 @@ public class RecyclerViewAdapterMainScreen extends RecyclerView.Adapter<Recycler
             public void onClick(View v) {
                 TextView txt = (TextView)v;
                 Log.i("SSS", txt.getText().toString());
-//                render(txt.getText().toString());
+                render(txt.getText().toString());
+//                showPdf(txt.getText().toString());
             }
         });
     }
 
-//    private void render(String fileName) {
-//
-//        try {
-//            ImageView mImageView = (ImageView)((Activity)mContext).findViewById(R.id.img_pdf_file);
-//            int REQ_WIDTH = 1000;//mImageView.getWidth();
-//            int REQ_HEIGHT = mImageView.getHeight() + 1000;
-//            int currentPage = 0;
-//
-//
-//
-//            Bitmap bitmap = Bitmap.createBitmap(REQ_WIDTH, REQ_HEIGHT, Bitmap.Config.ARGB_4444);
-//
-//            String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
-//            File file = new File(path + "/"+ fileName);
-//            PdfRenderer pdfRenderer = new PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY));
-//
-//            Matrix m = mImageView.getMatrix();
-//            Rect rec = new Rect(0,0,bitmap.getWidth(),bitmap.getHeight());
-//            pdfRenderer.openPage(currentPage).render(bitmap,rec, m, PdfRenderer.Page.RENDER_MODE_FOR_PRINT);
-//            mImageView.setImageMatrix(m);
-//            mImageView.setImageBitmap(bitmap);
-//            mImageView.invalidate();
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
+
+    private void showPdf(String name) {
+
+//        File file = new File(Environment.getExternalStorageDirectory()+"/Mypdf/Read.pdf");
+        String filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+        File file = new File(filepath + "/"+ name);
+
+        PackageManager packageManager = mContext.getPackageManager();
+        Intent testIntent = new Intent(Intent.ACTION_VIEW);
+        testIntent.setType("application/pdf");
+        List list = packageManager.queryIntentActivities(testIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "application/pdf");
+        mContext.startActivity(intent);
+    }
+
+
+    private void render(String fileName) {
+
+        try {
+            ImageView mImageView = (ImageView)((Activity)mContext).findViewById(R.id.img_pdf_file);
+            int REQ_WIDTH = 1000;//mImageView.getWidth();
+            int REQ_HEIGHT = mImageView.getHeight() + 1000;
+            int currentPage = 0;
+
+
+
+            Bitmap bitmap = Bitmap.createBitmap(REQ_WIDTH, REQ_HEIGHT, Bitmap.Config.ARGB_4444);
+
+            String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+            File file = new File(path + "/"+ fileName);
+            PdfRenderer pdfRenderer = new PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY));
+
+            Matrix m = mImageView.getMatrix();
+            Rect rec = new Rect(0,0,bitmap.getWidth(),bitmap.getHeight());
+            pdfRenderer.openPage(currentPage).render(bitmap,rec, m, PdfRenderer.Page.RENDER_MODE_FOR_PRINT);
+            mImageView.setImageMatrix(m);
+            mImageView.setImageBitmap(bitmap);
+            mImageView.invalidate();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     @Override
     public int getItemCount() {
